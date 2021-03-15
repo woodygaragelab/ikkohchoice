69c335064c626baa5722f9ca5290c47ad5f624f6
woodygaragelab <woodygaragelab@gmail.com>
2021-03-16 08:06:53 +0900

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+//import { API, Storage } from 'aws-amplify';
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+//import { listItems } from './graphql/queries';
+//import { createItem as createItemMutation, deleteItem as deleteItemMutation } from './graphql/mutations';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
 
 function App() {
   return (
